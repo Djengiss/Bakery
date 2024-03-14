5e1ca740a3cf459f0d4499c058f32133c0f76afd
Peter Haahr Bøgh Larsen <20083878@post.au.dk>
2024-03-14 10:42:06 +0100

    Created BakeryBatch

--- Bakery/Models/BakeryBatch.cs
+++ Bakery/Models/BakeryBatch.cs
@@ -2,5 +2,11 @@
 {
     public class BakeryBatch
     {
+        public DateOnly Date { get; set; }
+        public int Start_time { get; set; }
+        public int Target_end_time { get; set; }
+        public int End_time { get; set; }
+        public int Batch_id { get; set; }
+        public int Amount { get; set;}
     }
 }
