aaaf0410793b3234a397790195cddc3396c21b7e
FluffyAssassin <thea.lindgaard@gmail.com>
2024-03-14 10:33:35 +0100

    create baking good attributes

--- Bakery/Models/BakingGood.cs
+++ Bakery/Models/BakingGood.cs
@@ -2,5 +2,9 @@
 {
     public class BakingGood
     {
+        public int GoodsId { get; set; }
+        public string Recipe { get; set; }
+        public int DaysOfValidity { get; set; }
+        public string Name { get; set; }
     }
 }
