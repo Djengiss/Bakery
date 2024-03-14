7fdcef5cc6a182a02d27b7ae29b6a8eede546c1f
FluffyAssassin <thea.lindgaard@gmail.com>
2024-03-14 11:22:11 +0100

    typo in address attributes

--- Bakery/Models/Address.cs
+++ Bakery/Models/Address.cs
@@ -5,11 +5,11 @@ namespace Bakery.Models
     public class Address
     {
         [Key]
-        public int AdressId { get; set; }
+        public int AddressId { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
         public int Number { get; set; }
-        public List<Order>? Orders {  get; set; }
+        public List<Order>? Orders { get; set; }
         public List<DispatchRoute> DispatchRoutes { get; set; }
     }
 }
