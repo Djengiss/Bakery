b77a79d1e59712d7b02dda5715d8ed4094ada464
Niels Jakob H. OEsterberg <jacobaaa@hotmail.dk>
2024-03-18 15:50:25 +0100

    postal code added to adress

--- Bakery/Models/Address.cs
+++ Bakery/Models/Address.cs
@@ -6,6 +6,7 @@ namespace Bakery.Models
     {
         [Key]
         public int AddressId { get; set; }
+        public int PostalCode { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
         public int Number { get; set; }
