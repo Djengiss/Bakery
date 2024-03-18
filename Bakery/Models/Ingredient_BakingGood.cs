9169a1d1ac2ec083321f8e99fa90d11e3b64dedd
Peter Haahr Bøgh Larsen <20083878@post.au.dk>
2024-03-18 14:00:09 +0100

    2nd Migration

--- Bakery/Models/Ingredient_BakingGood.cs
+++ Bakery/Models/Ingredient_BakingGood.cs
@@ -1,13 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bakery.Models
 {
     public class Ingredient_BakingGood
     {
         [Key]
-        public int Ingredient_BakingGoo_Id { get; set; }
+        public int Ingredient_BakingGood_Id { get; set; }
+
+        [ForeignKey("Bakinggood")]
         public int GoodsId { get; set; }
+        public BakingGood Bakinggood { get; set; }
+
+        [ForeignKey("ingredient")]
         public int IngredientId { get; set; }
+        public Ingredients ingredient { get; set; }
         public int Amount { get; set; }
     }
 
